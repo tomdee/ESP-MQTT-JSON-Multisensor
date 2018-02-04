d91473dbda85809b4ace584244f6760af20e098d
Tom Denham <tom@tomdee.co.uk>
2018-02-03 19:15:00 -0800

    Add IR sensor and code to turn on my TV

--- bruh_mqtt_multisensor_github/bruh_mqtt_multisensor_github.ino
+++ bruh_mqtt_multisensor_github/bruh_mqtt_multisensor_github.ino
@@ -8,21 +8,21 @@
 
   Thanks much to @corbanmailloux for providing a great framework for implementing flash/fade with HomeAssistant https://github.com/corbanmailloux/esp-mqtt-rgb-led
 
-  To use this code you will need the following dependancies: 
-  
-  - Support for the ESP8266 boards. 
+  To use this code you will need the following dependancies:
+
+  - Support for the ESP8266 boards.
         - You can add it to the board manager by going to File -> Preference and pasting http://arduino.esp8266.com/stable/package_esp8266com_index.json into the Additional Board Managers URL field.
         - Next, download the ESP8266 dependancies by going to Tools -> Board -> Board Manager and searching for ESP8266 and installing it.
-  
+
   - You will also need to download the follow libraries by going to Sketch -> Include Libraries -> Manage Libraries
-      - DHT sensor library 
+      - DHT sensor library
       - Adafruit unified sensor
       - PubSubClient
       - ArduinoJSON
 
 */
 
-
+#define MQTT_MAX_PACKET_SIZE 512
 
 #include <ESP8266WiFi.h>
 #include <DHT.h>
@@ -31,21 +31,22 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <ArduinoJson.h>
-
+#include <ESP8266WebServer.h>
+#include <IRremoteESP8266.h>
 
 
 /************ WIFI and MQTT INFORMATION (CHANGE THESE FOR YOUR SETUP) ******************/
-#define wifi_ssid "YourSSID" //type your WIFI information inside the quotes
-#define wifi_password "YourWIFIpassword"
-#define mqtt_server "your.mqtt.server.ip"
-#define mqtt_user "yourMQTTusername" 
-#define mqtt_password "yourMQTTpassword"
+#define wifi_ssid ""
+#define wifi_password ""
+#define mqtt_server ""
+#define mqtt_user ""
+#define mqtt_password ""
 #define mqtt_port 1883
 
 
 /************* MQTT TOPICS (change these topics as you wish)  **************************/
-#define light_state_topic "bruh/sensornode1"
-#define light_set_topic "bruh/sensornode1/set"
+#define light_state_topic "bruh/sensornode3"
+#define light_set_topic "bruh/sensornode3/set"
 
 const char* on_cmd = "ON";
 const char* off_cmd = "OFF";
@@ -53,16 +54,13 @@ const char* off_cmd = "OFF";
 
 
 /**************************** FOR OTA **************************************************/
-#define SENSORNAME "sensornode1"
+#define SENSORNAME "sensornode3"
 #define OTApassword "bruh"
 int OTAport = 8266;
 
 
 
 /**************************** PIN DEFINITIONS ********************************************/
-const int redPin = D1;
-const int greenPin = D2;
-const int bluePin = D3;
 #define PIRPIN    D5
 #define DHTPIN    D7
 #define DHTTYPE   DHT22
@@ -92,45 +90,53 @@ int calibrationTime = 0;
 
 const int BUFFER_SIZE = 300;
 
-#define MQTT_MAX_PACKET_SIZE 512
 
 
-/******************************** GLOBALS for fade/flash *******************************/
-byte red = 255;
-byte green = 255;
-byte blue = 255;
-byte brightness = 255;
 
-byte realRed = 0;
-byte realGreen = 0;
-byte realBlue = 0;
+
 
 bool stateOn = false;
 
-bool startFade = false;
-unsigned long lastLoop = 0;
-int transitionTime = 0;
-bool inFade = false;
-int loopCount = 0;
-int stepR, stepG, stepB;
-int redVal, grnVal, bluVal;
 
-bool flash = false;
-bool startFlash = false;
-int flashLength = 0;
-unsigned long flashStartTime = 0;
-byte flashRed = red;
-byte flashGreen = green;
-byte flashBlue = blue;
-byte flashBrightness = brightness;
 
 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);
+ESP8266WebServer server(80);
 
+IRsend irsend(15);
 
+void handleRoot() {
+ server.send(200, "text/html", "<html><head> <title>ESP8266 Demo</title></head><body><h1>Hello from ESP8266, you can send NEC encoded IR signals from here!</h1><p><a href=\"ir?code=100334085\">Send TV Power</a></p><p><a href=\"ir?code=16712445\">Send Soundbar Power</a></p><p><a href=\"ir?code=16749420\">Send Soundbar digital input</a></p></body></html>");
+}
+
+void handleIr(){
+  for (uint8_t i=0; i<server.args(); i++){
+    if(server.argName(i) == "code") 
+    {
+      unsigned long code = strtoul(server.arg(i).c_str(), NULL, 10);
+      irsend.sendNEC(code, 32);
+    }
+  }
+  handleRoot();
+}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
 
 /********************************** START SETUP*****************************************/
 void setup() {
@@ -143,6 +149,20 @@ void setup() {
 
   Serial.begin(115200);
   delay(10);
+
+
+  irsend.begin();
+  server.on("/", handleRoot);
+  server.on("/ir", handleIr);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
 
   ArduinoOTA.setPort(OTAport);
 
@@ -163,7 +183,6 @@ void setup() {
 
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-
 
   ArduinoOTA.onStart([]() {
     Serial.println("Starting");
@@ -233,21 +252,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     return;
   }
 
-  if (stateOn) {
-    // Update lights
-    realRed = map(red, 0, 255, 0, brightness);
-    realGreen = map(green, 0, 255, 0, brightness);
-    realBlue = map(blue, 0, 255, 0, brightness);
-  }
-  else {
-    realRed = 0;
-    realGreen = 0;
-    realBlue = 0;
-  }
-
-  startFade = true;
-  inFade = false; // Kill the current fade
-
   sendState();
 }
 
@@ -273,55 +277,7 @@ bool processJson(char* message) {
     }
   }
 
-  // If "flash" is included, treat RGB and brightness differently
-  if (root.containsKey("flash")) {
-    flashLength = (int)root["flash"] * 1000;
 
-    if (root.containsKey("brightness")) {
-      flashBrightness = root["brightness"];
-    }
-    else {
-      flashBrightness = brightness;
-    }
-
-    if (root.containsKey("color")) {
-      flashRed = root["color"]["r"];
-      flashGreen = root["color"]["g"];
-      flashBlue = root["color"]["b"];
-    }
-    else {
-      flashRed = red;
-      flashGreen = green;
-      flashBlue = blue;
-    }
-
-    flashRed = map(flashRed, 0, 255, 0, flashBrightness);
-    flashGreen = map(flashGreen, 0, 255, 0, flashBrightness);
-    flashBlue = map(flashBlue, 0, 255, 0, flashBrightness);
-
-    flash = true;
-    startFlash = true;
-  }
-  else { // Not flashing
-    flash = false;
-
-    if (root.containsKey("color")) {
-      red = root["color"]["r"];
-      green = root["color"]["g"];
-      blue = root["color"]["b"];
-    }
-
-    if (root.containsKey("brightness")) {
-      brightness = root["brightness"];
-    }
-
-    if (root.containsKey("transition")) {
-      transitionTime = root["transition"];
-    }
-    else {
-      transitionTime = 0;
-    }
-  }
 
   return true;
 }
@@ -335,13 +291,6 @@ void sendState() {
   JsonObject& root = jsonBuffer.createObject();
 
   root["state"] = (stateOn) ? on_cmd : off_cmd;
-  JsonObject& color = root.createNestedObject("color");
-  color["r"] = red;
-  color["g"] = green;
-  color["b"] = blue;
-
-
-  root["brightness"] = brightness;
   root["humidity"] = (String)humValue;
   root["motion"] = (String)motionStatus;
   root["ldr"] = (String)LDR;
@@ -356,21 +305,6 @@ void sendState() {
 }
 
 
-/********************************** START SET COLOR *****************************************/
-void setColor(int inR, int inG, int inB) {
-  analogWrite(redPin, inR);
-  analogWrite(greenPin, inG);
-  analogWrite(bluePin, inB);
-
-  Serial.println("Setting LEDs:");
-  Serial.print("r: ");
-  Serial.print(inR);
-  Serial.print(", g: ");
-  Serial.print(inG);
-  Serial.print(", b: ");
-  Serial.println(inB);
-}
-
 
 
 /********************************** START RECONNECT*****************************************/
@@ -382,7 +316,6 @@ void reconnect() {
     if (client.connect(SENSORNAME, mqtt_user, mqtt_password)) {
       Serial.println("connected");
       client.subscribe(light_set_topic);
-      setColor(0, 0, 0);
       sendState();
     } else {
       Serial.print("failed, rc=");
@@ -395,7 +328,6 @@ void reconnect() {
 }
 
 
-
 /********************************** START CHECK SENSOR **********************************/
 bool checkBoundSensor(float newValue, float prevValue, float maxDiff) {
   return newValue < prevValue - maxDiff || newValue > prevValue + maxDiff;
@@ -405,6 +337,7 @@ bool checkBoundSensor(float newValue, float prevValue, float maxDiff) {
 
 /********************************** START MAIN LOOP***************************************/
 void loop() {
+  server.handleClient();
 
   ArduinoOTA.handle();
 
@@ -414,181 +347,46 @@ void loop() {
   client.loop();
 
 
-  if (!inFade) {
+  float newTempValue = dht.readTemperature(true);
+  float newHumValue = dht.readHumidity();
 
-    float newTempValue = dht.readTemperature(true);
-    float newHumValue = dht.readHumidity();
+  //PIR CODE
+  pirValue = digitalRead(PIRPIN); //read state of the
 
-    //PIR CODE
-    pirValue = digitalRead(PIRPIN); //read state of the
-
-    if (pirValue == LOW && pirStatus != 1) {
-      motionStatus = "standby";
-      sendState();
-      pirStatus = 1;
-    }
-
-    else if (pirValue == HIGH && pirStatus != 2) {
-      motionStatus = "motion detected";
-      sendState();
-      pirStatus = 2;
-    }
-
-    delay(100);
-
-    if (checkBoundSensor(newTempValue, tempValue, diffTEMP)) {
-      tempValue = newTempValue;
-      sendState();
-    }
-
-    if (checkBoundSensor(newHumValue, humValue, diffHUM)) {
-      humValue = newHumValue;
-      sendState();
-    }
-
-
-    int newLDR = analogRead(LDRPIN);
-
-    if (checkBoundSensor(newLDR, LDR, diffLDR)) {
-      LDR = newLDR;
-      sendState();
-    }
-
+  if (pirValue == LOW && pirStatus != 1) {
+    motionStatus = "standby";
+    sendState();
+    pirStatus = 1;
   }
 
-  if (flash) {
-    if (startFlash) {
-      startFlash = false;
-      flashStartTime = millis();
-    }
-
-    if ((millis() - flashStartTime) <= flashLength) {
-      if ((millis() - flashStartTime) % 1000 <= 500) {
-        setColor(flashRed, flashGreen, flashBlue);
-      }
-      else {
-        setColor(0, 0, 0);
-        // If you'd prefer the flashing to happen "on top of"
-        // the current color, uncomment the next line.
-        // setColor(realRed, realGreen, realBlue);
-      }
-    }
-    else {
-      flash = false;
-      setColor(realRed, realGreen, realBlue);
-    }
+  else if (pirValue == HIGH && pirStatus != 2) {
+    motionStatus = "motion detected";
+    sendState();
+    pirStatus = 2;
   }
 
-  if (startFade) {
-    // If we don't want to fade, skip it.
-    if (transitionTime == 0) {
-      setColor(realRed, realGreen, realBlue);
+  delay(100);
 
-      redVal = realRed;
-      grnVal = realGreen;
-      bluVal = realBlue;
-
-      startFade = false;
-    }
-    else {
-      loopCount = 0;
-      stepR = calculateStep(redVal, realRed);
-      stepG = calculateStep(grnVal, realGreen);
-      stepB = calculateStep(bluVal, realBlue);
-
-      inFade = true;
-    }
+  if (checkBoundSensor(newTempValue, tempValue, diffTEMP)) {
+    tempValue = newTempValue;
+    sendState();
   }
 
-  if (inFade) {
-    startFade = false;
-    unsigned long now = millis();
-    if (now - lastLoop > transitionTime) {
-      if (loopCount <= 1020) {
-        lastLoop = now;
-
-        redVal = calculateVal(stepR, redVal, loopCount);
-        grnVal = calculateVal(stepG, grnVal, loopCount);
-        bluVal = calculateVal(stepB, bluVal, loopCount);
-
-        setColor(redVal, grnVal, bluVal); // Write current values to LED pins
-
-        Serial.print("Loop count: ");
-        Serial.println(loopCount);
-        loopCount++;
-      }
-      else {
-        inFade = false;
-      }
-    }
+  if (checkBoundSensor(newHumValue, humValue, diffHUM)) {
+    humValue = newHumValue;
+    sendState();
   }
+
+
+  int newLDR = analogRead(LDRPIN);
+
+  if (checkBoundSensor(newLDR, LDR, diffLDR)) {
+    LDR = newLDR;
+    sendState();
+  }
+
+
+
 }
 
-
-
-
-/**************************** START TRANSITION FADER *****************************************/
-// From https://www.arduino.cc/en/Tutorial/ColorCrossfader
-/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
-
-  The program works like this:
-  Imagine a crossfade that moves the red LED from 0-10,
-    the green from 0-5, and the blue from 10 to 7, in
-    ten steps.
-    We'd want to count the 10 steps and increase or
-    decrease color values in evenly stepped increments.
-    Imagine a + indicates raising a value by 1, and a -
-    equals lowering it. Our 10 step fade would look like:
-
-    1 2 3 4 5 6 7 8 9 10
-  R + + + + + + + + + +
-  G   +   +   +   +   +
-  B     -     -     -
-
-  The red rises from 0 to 10 in ten steps, the green from
-  0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
-
-  In the real program, the color percentages are converted to
-  0-255 values, and there are 1020 steps (255*4).
-
-  To figure out how big a step there should be between one up- or
-  down-tick of one of the LED values, we call calculateStep(),
-  which calculates the absolute gap between the start and end values,
-  and then divides that gap by 1020 to determine the size of the step
-  between adjustments in the value.
-*/
-int calculateStep(int prevValue, int endValue) {
-  int step = endValue - prevValue; // What's the overall gap?
-  if (step) {                      // If its non-zero,
-    step = 1020 / step;          //   divide by 1020
-  }
-
-  return step;
-}
-
-/* The next function is calculateVal. When the loop value, i,
-   reaches the step size appropriate for one of the
-   colors, it increases or decreases the value of that color by 1.
-   (R, G, and B are each calculated separately.)
-*/
-int calculateVal(int step, int val, int i) {
-  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
-    if (step > 0) {              //   increment the value if step is positive...
-      val += 1;
-    }
-    else if (step < 0) {         //   ...or decrement it if step is negative
-      val -= 1;
-    }
-  }
-
-  // Defensive driving: make sure val stays in the range 0-255
-  if (val > 255) {
-    val = 255;
-  }
-  else if (val < 0) {
-    val = 0;
-  }
-
-  return val;
-}
 
